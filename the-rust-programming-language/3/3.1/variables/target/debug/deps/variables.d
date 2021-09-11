0362c50ff5780349ffd197be8e2a40993956a8b0
Riley Gowan <t.rileygowan@gmail.com>
2021-09-11 18:53:40 -0400

    3.1

---
+++ the-rust-programming-language/3/3.1/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/rgowan/Repos/Personal/rust/the-rust-programming-language/3/3.1/variables/target/debug/deps/variables: src/main.rs
+
+/Users/rgowan/Repos/Personal/rust/the-rust-programming-language/3/3.1/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ the-rust-programming-language/3/3.1/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/rgowan/Repos/Personal/rust/the-rust-programming-language/3/3.1/variables/target/debug/variables: /Users/rgowan/Repos/Personal/rust/the-rust-programming-language/3/3.1/variables/src/main.rs
