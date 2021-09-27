171d1d5728c2fd797649920bda814a0d65725383
Riley Gowan <t.rileygowan@gmail.com>
2021-09-27 08:58:14 -0400

    Finish 8.1

--- the-rust-programming-language/8/8.1/vector/src/main.rs
+++ the-rust-programming-language/8/8.1/vector/src/main.rs
@@ -59,11 +59,25 @@
 //     }
 // }
 
+// fn main() {
+//     let mut v = vec![100, 32, 57];
+//     for i in &mut v {
+//         *i += 50;
+//     }
+
+//     println!("{:?}", v)
+// }
+
 fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
 
-    println!("{:?}", v)
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
